0ba8d54e4da630459358babce3ff2dc8742a6bbb
AchroDev <achrodev@proton.me>
2024-09-20 22:57:21 -0500

    Refactored code using static example

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 // Using a struct to have default publicity with variables
 struct Entity
 {
-    int x, y;
+    static int x, y; // making them static
 
     // simple function
     void Print()
@@ -22,7 +22,11 @@ int main()
     e.y = 3;
 
     // Instantiating the struct using an initializer
-    Entity e1 = {5, 8};
+    // Entity e1 = {5, 8}; // Due to the variables being static, the initializer will fail.
+
+    Entity e1; // Since x and y are "no longer" class members the code would need to be written like this.
+    e1.x = 5;
+    e1.y = 8;
 
     e.Print();
     e1.Print();
