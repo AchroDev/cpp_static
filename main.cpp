d091a86cde42752ac736e4e9a900fc7c9922b9a5
AchroDev <achrodev@proton.me>
2024-09-20 23:20:07 -0500

    Refactored code to modify the data in the variables "correctly"

--- main.cpp
+++ main.cpp
@@ -18,15 +18,15 @@ int main()
 {
     // Instantiating the struct
     Entity e;
-    e.x = 2;
-    e.y = 3;
+    Entity::x = 2; // re-written the correct way
+    Entity::y = 3;
 
     // Instantiating the struct using an initializer
     // Entity e1 = {5, 8}; // Due to the variables being static, the initializer will fail.
 
-    Entity e1; // Since x and y are "no longer" class members the code would need to be written like this.
-    e1.x = 5;
-    e1.y = 8;
+    Entity e1;     // Since x and y are "no longer" class members the code would need to be written like this.
+    Entity::x = 5; // Now that it's re-written you should understand why 5 and 8 print twice.
+    Entity::y = 8;
 
     e.Print();
     e1.Print();
